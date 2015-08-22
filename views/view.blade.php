61686880a8802a1b58ba97e60b514e2cdfaec3c0
Oleg <malezha@e-mail.ua>
2015-08-22 15:46:23 +0300

    Забыл

--- views/view.blade.php
+++ views/view.blade.php
@@ -4,7 +4,10 @@
             @if ($item instanceof \Malezha\Menu\Item)
                 <li><a href="{{ $item->getUrl() }}" {!! $item->buildAttributes() !!}>{!! $item->getTitle() !!}</a></li>
             @else
-                @include(config('menu.view'), ['menu' => $item]))
+                <li {!! $item->buildAttributes($item->getGroup()->attributes) !!}>
+                    <a href="{{ $item->getGroup()->url }}">{!! $item->getGroup()->title !!}</a>
+                    @include(config('menu.view'), ['menu' => $item])
+                </li>
             @endif
         @endforeach
     </{{ $menu->getType() }}>
