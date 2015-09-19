15f0514368242eb3bf52d1552497b5cf6da93407
Malezha <malezha@e-mail.ua>
2015-09-19 16:27:48 +0300

    fix template (2)

--- src/views/view.blade.php
+++ src/views/view.blade.php
@@ -4,7 +4,7 @@
             @if ($item instanceof \Malezha\Menu\Item)
                 <li{!! $item->buildAttributes() !!}><a href="{{ $item->link()->url() }}"{!! $item->link()->buildAttributes() !!}>{!! $item->link()->title() !!}</a></li>
             @elseif ($item instanceof \Malezha\Menu\Group)
-                <li{!! $item->getItem()->buildAttributes() !!}>
+                <li{!! $item->item()->buildAttributes() !!}>
                     <a href="{{ $item->item()->link()->url() }}{!! $item->item()->link()->buildAttributes() !!}">{!! $item->item()->link()->title() !!}</a>
                     @include(config('menu.view'), ['menu' => $item->menu()])
                 </li>
